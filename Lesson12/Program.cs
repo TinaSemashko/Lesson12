da8ab13fc7a976ebd2de1504d5b8c79dcdc7ee51
TinaSemashko <akateria.satory@gmail.com>
2022-08-31 18:24:15 +0200

    Add project files.

---
+++ Lesson12/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        AutoService BestService = new AutoService("BestService", 123456);
+        BestService.Print();
+        Vehicle Transp = new Vehicle("BestService", 123456, "light", 4444444);
+        Transp.PrintReport();
+        Wheels Teach1 = new Wheels("BestService", 123456, "rubber", 5555555);
+        Car Schedule1 = new Car("BestService", 123456, "light", 4444444, 00010001);
+    }
+}
+
+class AutoService
+{
+    string Name { get; set; }
+    int Id { get; set; }
+    public AutoService(string name, int id)
+    {
+        Name = name;
+        Id = id;
+    }
+    public void Print()
+    {
+        Console.WriteLine($"AutoService {Name}");
+    }
+}
+class Vehicle : AutoService
+
+{
+    string Type { get; set; }
+    int Number { get; set; }
+
+    public Vehicle(string name, int id, string type, int number) : base(name, id)
+    {
+        Type = type;
+        Number = number;
+    }
+
+    public void PrintReport()
+    {
+        Console.WriteLine("Car " + Type + " number " + Number);
+    }
+}
+
+    class Wheels : AutoService
+{
+    DateTime DataOfManufacture;
+    string Matherial { get; set; }
+    int IdWheel { get; set; }
+
+    public Wheels(string name, int id, string matherial, int idWheel) : base(name, id)
+    {
+        Matherial = matherial;
+        IdWheel = idWheel;
+    }
+}
+
+class Car: Vehicle
+{
+    int Seria { get; set; }
+
+    public Car(string name, int id, string type, int number, int seria) : base(name, id, type, number)
+    {
+        Seria = seria;
+    }
+}
